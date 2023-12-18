132169ae4ede224580c60cda6bf8f68b9426cafc
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-12-18 13:29:05 -0500

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,33 +2,123 @@ class Game:
     def __init__(self, title):
         self.title = title
 
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if hasattr(self, "title"):
+            raise Exception("cannot change title")
+        elif isinstance(title, str) and len(title) > 0:
+            self._title = title
+        else:
+            raise Exception("titles must be strings longer than 0 chars")
+
     def results(self):
-        pass
+        return [
+            result
+            for result in Result.all
+            if result.game == self and isinstance(result, Result)
+        ]
 
     def players(self):
-        pass
+        allplayers = [result.player for result in self.results()]
+        return list(set(allplayers))
 
     def average_score(self, player):
-        pass
+        scores = [result.score for result in self.results() if result.player == player]
+        return sum(scores) / len(scores) if len(scores) else 0
+
 
 class Player:
     def __init__(self, username):
         self.username = username
 
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and len(username) in range(2, 17):
+            self._username = username
+        else:
+            raise Exception("username must be string between 2-16 chars")
+
     def results(self):
-        pass
+        return [
+            result
+            for result in Result.all
+            if result.player == self and isinstance(result, Result)
+        ]
 
     def games_played(self):
-        pass
+        allgames = [result.game for result in self.results()]
+        return list(set(allgames))
 
     def played_game(self, game):
-        pass
+        return game in self.games_played()
 
     def num_times_played(self, game):
-        pass
+        allgames = [result.game for result in self.results()]
+        return allgames.count(game)
+
+    @classmethod
+    def highest_scored(cls, game):
+        players = game.players()
+        if not players:
+            return None
+        averages = [
+            {"player": player, "score": game.average_score(player)}
+            for player in players
+        ]
+        sorted_scores = sorted(averages, key=lambda obj: obj["score"])
+        return sorted_scores[-1]["player"]
+
 
 class Result:
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        type(self).all.append(self)
+
+    all = []
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        if (
+            isinstance(score, int)
+            and score in range(1, 5001)
+            and not hasattr(self, "score")
+        ):
+            self._score = score
+        else:
+            raise Exception("score must be int between 1-5000 and cannot be changed")
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+        else:
+            raise Exception("player must be an instance of Player")
+
+    @property
+    def game(self):
+        return self._game
+
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self._game = game
+        else:
+            raise Exception("game must be an instance of Game")
--- lib/debug.py
+++ lib/debug.py
@@ -5,7 +5,17 @@ from classes.many_to_many import Player
 from classes.many_to_many import Game
 from classes.many_to_many import Result
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("HELLO! :) let's debug :vibing_potato:")
 
+    game1 = Game("blockus")
+    game2 = Game("sorry")
+    game3 = Game("scrabble")
+    player1 = Player("sammy")
+    player2 = Player("bob")
+    result1 = Result(player1, game1, 10)
+    result2 = Result(player2, game2, 15)
+    result3 = Result(player1, game3, 20)
+    result4 = Result(player1, game1, 110)
+    result5 = Result(player2, game1, 50)
     ipdb.set_trace()
